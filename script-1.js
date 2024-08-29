c965e0efb03282cba26601f1c60a2527d6efd8de
SalvatoreGiacomazzo <giacomazzo161@gmail.com>
2024-08-29 17:50:30 +0200

    snack 2 finished

--- script-1.js
+++ script-1.js
@@ -11,3 +11,41 @@ nome dell'ospite (guestName),
 posto occupato (place),
 Generiamo e stampiamo in console la lista per i segnaposto.
 */
+
+
+//Creazione dell'array con i nomi degli ospiti
+
+const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'George Clooney',
+    'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+
+
+//Dichiarazione dell'array di oggetti finale 
+const listPlaceholders = [];
+const tableName = 'Tavolo Vip';
+
+//Creazione lista 
+guests.forEach((guest, index) => {   //Ciclo foreach perché è un'operazione ciclica
+    listPlaceholders.push({
+        table: tableName,       //Assegno i value richiesti
+        name: guest,
+        place: index + 1
+
+    })
+})
+console.log(listPlaceholders)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
